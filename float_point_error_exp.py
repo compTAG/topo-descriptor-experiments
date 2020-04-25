f183549198f03695225c9814a17b1bb24f328b21
Samuel Micka <mickas37@gmail.com>
2020-04-25 17:49:32 -0600

    Move away from forced formating

--- float_point_error_exp.py
+++ float_point_error_exp.py
@@ -7,7 +7,7 @@ from main import get_exp_graphs
 # @param Networkx Graph G
 # @param String outFile: the file to write experiment results to
 def error_exp(G, outFile):
-	with open("output_001_approx/error_exp/"+outFile, "w+") as f:
+	with open("output_005_approx/error_exp/"+outFile, "w+") as f:
 		minimum = min([v[1]['v'].get_x() for v in list(G.nodes(data=True))])
 		maximum = max([v[1]['v'].get_x() for v in list(G.nodes(data=True))])
 		width = maximum-minimum
@@ -16,12 +16,12 @@ def error_exp(G, outFile):
 		height = min(np.diff(yVals))
 
 		f.write("n,width,height\n")
-		f.write("%d,%.16f,%.16f"%(len(G.nodes()), width, height))
+		f.write(str(len(G.nodes()))+","+str(width)+","+str(height))
 
 
 
 def main():
-	data_type = 3 #1 for random, 2 for mpeg7, 3 for mnist, 4 for all data types
+	data_type = 1 #1 for random, 2 for mpeg7, 3 for mnist, 4 for all data types
 	exp_list = get_exp_graphs(data_type)
 	print(len(exp_list))
 	for e in exp_list:
