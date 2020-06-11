4d2f593fb82387db96b60b8e8ef6b34f6312fd29
David Millman <dave@cs.unc.edu>
2020-06-11 15:36:26 -0600

    Refactor data fetching

--- get_data.py
+++ get_data.py
@@ -5,66 +5,77 @@ import wget
 import os
 from zipfile import ZipFile
 
+URL_MPEG7 = 'http://www.dabi.temple.edu/~shape/MPEG7/MPEG7dataset.zip'
+URL_EMNIST = 'http://www.itl.nist.gov/iaui/vip/cs_links/EMNIST/matlab.zip'
+
 dir_data = 'data'
 
-dir_emnist_005 = 'graphs_005_approx/mnist'
-dir_img_emnist_005 = 'graphs_005_approx/mnist_imgs'
+dir_list = [
+    dir_data,
+    os.path.concat('graphs_005_approx','mnist'),
+    'graphs_005_approx/mnist_imgs',
+    'graphs_001_approx/mnist',
+    'graphs_001_approx/mnist_imgs',
+    'graphs_005_approx/mpeg7',
+    'graphs_005_approx/mpeg7_imgs',
+    'graphs_005_approx/mpeg7_extra',
+    'graphs_001_approx/mpeg7',
+    'graphs_001_approx/mpeg7_imgs',
+    'graphs_001_approx/mpeg7_extra',
+]
 
-dir_emnist_001 = 'graphs_001_approx/mnist'
-dir_img_emnist_001 = 'graphs_001_approx/mnist_imgs'
-
-dir_mpeg7_005 = 'graphs_005_approx/mpeg7'
-dir_img_mpeg7_005 ='graphs_005_approx/mpeg7_imgs'
-dir_mpeg7_extra_005 ='graphs_005_approx/mpeg7_extra'
-
-dir_mpeg7_001 = 'graphs_001_approx/mpeg7'
-dir_img_mpeg7_001 ='graphs_001_approx/mpeg7_imgs'
-dir_mpeg7_extra_001 ='graphs_001_approx/mpeg7_extra'
-
-dir_list =[dir_data, dir_emnist_005, dir_img_emnist_005, dir_emnist_001, dir_mpeg7_005, 
-dir_img_mpeg7_005, dir_mpeg7_extra_005, dir_mpeg7_001,dir_img_mpeg7_001,dir_mpeg7_extra_001]
 
 # Make directories so that generate_graphs runs properly
 def make_folders(dir_list):
   for x in dir_list:
-    if not os.path.exists(x):
-      os.makedirs(x)
-      print("Directory " , x ,  " Created ")
-    else:    
-      print("Directory " , x,  " already exists")
+    make_folder(x)
+    
+def make_folder(folder_path):
+  if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+    print("Directory %s created" % folder_path)
+  else:    
+    print("Directory %s already exists" % folder_path)
 
+    
+def get_data(url, target_dir, data_set_name):
+  # first check if we should do any downloading
+  if os.path.exists(target_dir):
+    print("Warning: directory %s alreday exists, not redownloading %s dataset" % target_dir, data_set_name)
+    return
+
+  data = wget.download(url)
+
+  # Unzip data and store in data folder
+  with ZipFile(mpeg7_data, 'r') as zip_ref:
+    # TODO: python has a set of tools for creating tmp paths for file.
+    # can we rename with extractall
+    # can we get the name without assumeing "original"
+    # when concating paths, use os.path.concat(...)
+    tmp = 'tmp'
+    zip_ref.extractall(tmp)
+
+  # move
+  dst = target_dir + data_set_name
+  os.rename(tmp+'original', dst)
+  print("Directory " , dst,  " Created ")
+  
+  # cleanup tmp  
+  os.remove(tmp)
+
+  return dst
+    
+def get_mpeg7(url, target_dir):
+  dst = get_data(url, target_dir, 'mpeg7')
+  
+  # clean bad files
+  os.remove(dst + 'rat-09.gif')
+    
+
+def get_emnist(url, target_dir):
+  dst = get_data(url, target_dir, 'emnist')
+    
 
 def download_data():
-  # Data url's for mpeg7 and emnist data 
-  url_mpeg7 = 'http://www.dabi.temple.edu/~shape/MPEG7/MPEG7dataset.zip'
-  url_emnist = 'http://www.itl.nist.gov/iaui/vip/cs_links/EMNIST/matlab.zip'
-
-  # Grab the data with wget
-  mpeg7_data = wget.download(url_mpeg7)
-  emnist_data = wget.download(url_emnist)
-
-  # Unzip mpeg7 data and store in data folder
-  with ZipFile(mpeg7_data, 'r') as zip_ref:
-    zip_ref.extractall('data')
-
-  if not os.path.exists('data/mpeg7'):
-    os.rename('data/original', 'data/mpeg7')
-    print("Directory " , 'data/mpeg7',  " Created ")
-    # remove RAT-09 file
-    os.remove('data/mpeg7/rat-09.gif')
-    os.remove('MPEG7dataset.zip')
-  else:    
-    print("Directory " , 'data/mpeg7' ,  " already exists")
-
-  # Unzip emnist data and store in data folder
-  with ZipFile(emnist_data, 'r') as zip_ref:
-    # Extract all the contents of zip file in different directory
-    zip_ref.extractall('data')
-
-  if not os.path.exists('data/emnist'):
-    os.rename('data/matlab', 'data/emnist')
-    print("Directory " , 'data/emnist',  " Created ")
-    os.remove('matlab.zip')
-  else:    
-    print("Directory " , 'data/emnist' ,  " already exists")  
-
+  get_mpeg7(URL_MPEG7, data_dir)
+  get_emnist(URL_EMNIST, data_dir)
