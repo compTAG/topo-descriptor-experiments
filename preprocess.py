72801db057b51565e683fb379d7ba561a40a9b69
James-B-Wilson <jbwilson1982@gmail.com>
2020-06-12 13:15:05 -0600

    Update preprocess to run get_data and generate_graphs

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,15 @@
 from get_data import *
 from generate_graphs import *
+from tqdm import tqdm 
 
-def preprocess_data(directories):
-  make_folders(directories)
-  download_data()
-  main()
+#def preprocess_data(directories):
+ # make_folders(directories)
+  #download_data()
+  #main()
+
+
+make_folders(dir_list)
+get_mpeg7(URL_MPEG7, dir_data)
+get_emnist(URL_EMNIST, dir_data)
+main()
+
