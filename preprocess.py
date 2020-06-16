c6ddfc488181f7359c7b943f94f29aff5a800c6a
James-B-Wilson <jbwilson1982@gmail.com>
2020-06-16 15:21:22 -0600

    Updated to just create folders and download data

--- preprocess.py
+++ preprocess.py
@@ -1,15 +1,16 @@
 from get_data import *
-from generate_graphs import *
 from tqdm import tqdm 
 
-#def preprocess_data(directories):
- # make_folders(directories)
-  #download_data()
-  #main()
+
+# Should this step also generate the output folders for running experiments? 
 
 
-make_folders(dir_list)
-get_mpeg7(URL_MPEG7, dir_data)
-get_emnist(URL_EMNIST, dir_data)
-main()
+# Generate appropriate folders for data processing and download data.
+def preprocess_data(dir_list):
+  make_folders(dir_list)
+  download_data()
+
+preprocess_data(dir_list)
+
+
 
