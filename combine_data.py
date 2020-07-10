1d41ed71b1b4b8ec2d5192f8cec03cecf099ad8d
James-B-Wilson <jbwilson1982@gmail.com>
2020-07-10 15:05:11 -0600

    File to combine all output data for both experiments

--- combine_data.py
+++ combine_data.py
@@ -24,43 +24,93 @@ def combine_pngs(exp,approx):
   new_im.save(os.path.join('figs', exp, exp + '_' + approx +'.png'))
 
 
-def find_stats(in_file):
+def find_stats(in_file,exp):
   with open(in_file) as f:
     stats = csv.DictReader(f)
     stats_row = next(stats)
-    return {"n": stats_row['n'],
-        "minSize":stats_row['min_angle'],
-        "fineStratum": stats_row['num_stratum'],
-        "necessaryStratum": stats_row['num_needed_stratum']
-        }
+    if exp == "smallest_angle_exp":
+      return {"n": stats_row['n'],
+          "minSize":stats_row['min_angle'],
+          "fineStratum": stats_row['num_stratum'],
+          "necessaryStratum": stats_row['num_needed_stratum']
+          }
+    else:
+      for row in stats:
+      # grab the right row and trim out data that has too many stratum
+        if int(row['samples'])==16384 and int(row['num_stratum']) <= 5000:
+          return {"n": row['n'],
+            "samples":row['samples'],
+            "hits": row['hits'],
+            "num_stratum": row['num_stratum']
+            }
+      return -1
 
-def write_stats(error_stats,out_file):
-  with open(out_file, 'w') as f:
-    f.write("n,minSize,fineStratum,necessaryStratum\n")
-    for graph in error_stats:
-      f.write(str(graph['n'])+","+
-          str(graph['minSize'])+","+
-          str(graph['fineStratum'])+","+
-          str(graph['necessaryStratum'])+"\n")
+def write_stats(error_stats,out_file, exp):
+  if exp == "smallest_angle_exp":
+    with open(out_file, 'w') as f:
+            f.write("n,minSize,fineStratum,necessaryStratum\n")
+            for graph in error_stats:
+                    f.write(str(graph['n'])+","+
+                                    str(graph['minSize'])+","+
+                                    str(graph['fineStratum'])+","+
+                                    str(graph['necessaryStratum'])+"\n")
+  else: 
+    with open(out_file, 'w') as f:
+            f.write("n,samples,hits,num_stratum\n")
+            for graph in error_stats:
+                    f.write(str(graph['n'])+","+
+                                    str(graph['samples'])+","+
+                                    str(graph['hits'])+","+
+                                    str(graph['num_stratum'])+"\n")
 
 
 def random(exp, approx, file_name):
-  num_points = [3, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-  for k in num_points:
-    error_stats = []
-    out_file = os.path.join("analysis_" + approx + "_approx", exp, "combined_data", "random", file_name + "_" + str(k)+".txt")
-    for i in range(0,100):
-      in_file = os.path.join('output_' + approx + '_approx', exp, 'random', 'RAND_'+str(k)+"_"+str(i)+".txt")
-      error_stats.append(find_stats(in_file))
-    write_stats(error_stats, out_file)
+  if exp == "smallest_angle_exp":
+    num_points = [3, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    for k in num_points:
+      error_stats = []
+      out_file = os.path.join("analysis_" + approx + "_approx", exp, "combined_data", "random", file_name + "_" + str(k)+".txt")
+      for i in range(0,100):
+        in_file = os.path.join('output_' + approx + '_approx', exp, 'random', 'RAND_'+str(k)+"_"+str(i)+".txt")
+        error_stats.append(find_stats(in_file,exp))
+      write_stats(error_stats, out_file, exp)
+  else:
+    num_points = [3, 5, 10, 20, 30, 40, 50, 60, 70]
+    for k in num_points:
+      error_stats = []
+      out_file = os.path.join("analysis_" + approx + "_approx", exp, "combined_data", "random", file_name + "_" + str(k)+".txt")
+      for i in range(0,100):
+              in_file = os.path.join('output_' + approx + '_approx', exp, 'random', 'RAND_'+str(k)+"_"+str(i)+".txt")
+              stats = find_stats(in_file,exp)
+              if stats != -1:
+                    error_stats.append(stats)
+    # print(len(error_stats))
+      write_stats(error_stats, out_file,exp)
+
 
 def mpeg7_mnist(data_type, exp, approx,file_name):
-  error_stats = []
-  out_file = os.path.join('analysis_' + approx + '_approx', exp, 'combined_data', data_type, file_name + '.txt')
-  for filename in os.listdir(os.path.join('output_' + approx + '_approx', exp, data_type)):
-    in_file = os.path.join('output_' + approx + '_approx', exp, data_type, filename)
-    error_stats.append(find_stats(in_file))
-  write_stats(error_stats, out_file)
+  if exp == "smallest_angle_exp":
+    error_stats = []
+    out_file = os.path.join('analysis_' + approx + '_approx', exp, 'combined_data', data_type, file_name + '.txt')
+    for filename in os.listdir(os.path.join('output_' + approx + '_approx', exp, data_type)):
+      in_file = os.path.join('output_' + approx + '_approx', exp, data_type, filename)
+      error_stats.append(find_stats(in_file,exp))
+    write_stats(error_stats, out_file, exp)
+  else:
+    error_stats = []
+    out_file = os.path.join('analysis_' + approx + '_approx', exp, 'combined_data', data_type, file_name + '.txt')
+    for filename in os.listdir(os.path.join('output_' + approx + '_approx', exp, data_type)):
+      in_file = os.path.join('output_' + approx + '_approx', exp, data_type, filename)
+      stats = find_stats(in_file,exp)
+      if stats != -1:
+        error_stats.append(stats)
+      else:
+        print "Error"
+        print(in_file)
+    # print(len(error_stats))
+    write_stats(error_stats, out_file,exp)
+
+
 
 #def main():
   #random("smallest_angle_exp", "001", 'angle_stats')
