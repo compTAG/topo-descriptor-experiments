3daac1edb1807969bc879f97ee53baada3dab3e5
David Millman <dave@cs.unc.edu>
2020-12-24 15:03:37 -0700

    Implement lower start filtration

--- topology/__init__.py
+++ topology/__init__.py
@@ -1,7 +1,53 @@
+import dionysus
+
+
+class Dot(object):
+    def __init__(self, verts, direction):
+        self._verts = verts
+        self._dir = direction
+
+    def __call__(self, idx):
+        v = self._verts[idx]['pos']
+        return self._dir[0]*v[0] + self._dir[1]*v[1]
+
+
+class MaxHeight(object):
+    def __init__(self, heights):
+        self._heights = heights
+
+    def __call__(self, simplex):
+        return max(map(lambda i: self._heights[i], simplex))
+
+
 class LowerStarFiltrationFactory(object):
+    def _compute_function_vals(self, verts):
+        dot = Dot(verts, self._dir)
+        return list(map(dot, verts))
+
+    def _create_simplices(self, graph, f):
+        simp0 = map(lambda v: ([v], f[v]), graph.nodes)
+
+        fe = MaxHeight(f)
+        simp1 = map(lambda e: (list(e), fe(e)), graph.edges)
+
+        return list(simp0) + list(simp1)
 
     def __init__(self, direction):
         self._dir = direction
 
     def create(self, graph):
+        f = self._compute_function_vals(graph.nodes)
+        simplices = self._create_simplices(graph, f)
+
+
+        filtr = dionysus.Filtration()
+        for vertices, time in simplices:
+            simplex = dionysus.Simplex(vertices, time)
+            filtr.append(simplex)
+
+        filtr.sort()
+        return filtr
+
+
+
         return None
--- topology/test.py
+++ topology/test.py
@@ -5,6 +5,12 @@ import networkx as nx
 import topology
 
 class TestLowerStartFiltrationFactory(unittest.TestCase):
+    def assertEqualEdge(self, e, s):
+        self.assertTrue(len(s) == 2 and e == (s[0], s[1]))
+
+    def assertEqualVert(self, v, s):
+        self.assertTrue(len(s) == 1 and v == s[0])
+
 
     def create_graph(self):
         g = nx.Graph()
@@ -41,5 +47,17 @@ class TestLowerStartFiltrationFactory(unittest.TestCase):
         graph = self.create_graph()
 
         fltr = topology.LowerStarFiltrationFactory(direction).create(graph)
-        self.assertTrue(True)
 
+        self.assertEqualVert(0, fltr[0])
+        self.assertEqualVert(2, fltr[1])
+        self.assertEqualEdge((0, 2), fltr[2])
+        self.assertEqualVert(1, fltr[3])
+        self.assertEqualEdge((0, 1), fltr[4])
+        self.assertEqualEdge((1, 2), fltr[5])
+        self.assertEqualVert(3, fltr[6])
+        self.assertEqualEdge((1, 3), fltr[7])
+        self.assertEqualVert(4, fltr[8])
+        self.assertEqualEdge((3, 4), fltr[9])
+        self.assertEqualVert(5, fltr[10])
+        self.assertEqualEdge((3, 5), fltr[11])
+        self.assertEqualEdge((4, 5), fltr[12])
