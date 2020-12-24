8bab1edad5e3febf7d823fa82749eac2e5e3c774
David Millman <dave@cs.unc.edu>
2020-12-24 14:57:45 -0700

    Create input graph

--- topology/test.py
+++ topology/test.py
@@ -1,9 +1,41 @@
 import unittest
 
+import networkx as nx
 
-class HelloWold(unittest.TestCase):
+class TestLowerStartFiltrationFactory(unittest.TestCase):
 
-    def test_system_setup(self):
-        self.assertTrue(True)
+    def create_graph(self):
+        g = nx.Graph()
+
+        vertex = (
+            (0,0),
+            (2,2),
+            (1,3),
+            (4,1),
+            (5,2),
+            (6,1),
+        )
+
+        edges = (
+            (0, 1),
+            (1, 2),
+            (2, 0),
+            (1, 3),
+            (3, 5),
+            (5, 4),
+            (4, 3),
+        )
+
+        g.add_nodes_from(range(len(vertex)))
+        for i, coords in enumerate(vertex):
+            g.nodes[i]['pos'] = coords
+
+        g.add_edges_from(edges)
+
+        return g
+
+    def test_create(self):
+        direction = (1,0)
+        graph = self.create_graph()
 
 
