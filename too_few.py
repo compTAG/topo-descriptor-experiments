85e0ef200c7cf53303c8ae6f5cb12067cdf88ddc
James-B-Wilson <jbwilson1982@gmail.com>
2021-03-07 13:20:37 -0700

    Clean up code

--- too_few.py
+++ too_few.py
@@ -7,7 +7,7 @@ import topology
 import dionysus as d
 import planarity as p
 import matplotlib.pyplot as plt
-from planar_graphs import get_source_graph, plot_graphs, create_graph, circle_disc, is_intersection, clean_planar,find_all_planar_graphs, make_graphs
+from planar_graphs import get_source_graph, plot_graphs, create_graph, circle_disc, is_intersection, find_planar_graphs
 import itertools
 import pickle
 import glob
@@ -18,12 +18,9 @@ import random
 class DirectionalExp(object):
   def __init__(self, G, verts, directions):
       self.graph = create_graph(verts, G.edges())
-      self.main_diagram = topology.DirectionalDiagram(self.graph, (0,1))
       self.directional_diagrams = []
       self.verts = verts
-      self.planar = find_all_planar_graphs(list(itertools.combinations(G.nodes(), 2)))
-      self.graphs = clean_planar(self.verts, self.planar)
-      #self.graphs = make_graphs(len(G.nodes()))
+      self.graphs = find_planar_graphs(self.verts,list(itertools.combinations(G.nodes(), 2)))
       self.planar_graphs = []
       self.pos = { i : verts[i] for i in range(0, len(verts) ) }
       self.directions = directions
@@ -32,7 +29,8 @@ class DirectionalExp(object):
 
   def plot_planar(self):
     plot_graphs(self.graphs[1:-1])
-  #and (g.edges() != self.graph.edges()
+
+
   def clean(self):
     self.graphs.pop(0)
     for graph in self.graphs:
@@ -79,15 +77,11 @@ class DirectionalExp(object):
 
   def find_num_directions(self):
     max_count = max(map(lambda i: i.count, self.planar_graphs))
-    #print(max_count)
+
     for n in range(max_count):
       self.num_directions.append(sum(map(lambda i: i.count >= n, self.planar_graphs)))
     
     self.alpha = len(self.num_directions)-1
-    #if len(self.directions) > 1:
-    #self.alpha = self.num_directions[-1]
-    #print(self.num_directions)
-      #self.num_directions.index(1)
     
   def plot_num_directions(self):
     max_count = max(map(lambda i: i.count, self.planar_graphs))
@@ -102,28 +96,14 @@ class DirectionalExp(object):
     
 
   def planar_exp(self):
-    #self.plot_planar()
     self.clean()
     self.create_diagrams()
     self.fill()
-    #self.find_num_directions()
 
   def bottlenecks(self):
     dgm_1 = self.directional_diagrams[0]
     dgm_2 = self.directional_diagrams[1]
     b = d.bottleneck_distance(dgm_1._dgms[0], dgm_2._dgms[0])
-    print(b)
-
-class PlanarGraph(object):
-  def __init__(self, graph, directions):
-    self.graph = graph.graph
-    self.diagrams = []
-    self.directions = directions
-
-  def build_diagrams(self):
-    for direction in self.directions:
-      dgm = topology.DirectionalDiagram(self.graph, direction)
-      self.diagrams.append(dgm)
 
 class ExpGraph(object):
     def __init__(self, graph):
@@ -132,63 +112,30 @@ class ExpGraph(object):
 
 
 if __name__ == "__main__":
-  '''
-  source_dir = os.path.join("graphs","maps","Bozeman","graphs","5_graphs", "5_nodes.pickle")
+  
+  source_dir = os.path.join("graphs","maps","Bozeman","graphs","4_graphs", "4_nodes.pickle")
   in_graph = open(source_dir,"rb")
   graphs = pickle.load(in_graph)
+  print(len(graphs))
   
-  experiment = []
-  directions = circle_disc(10)
-  for graph in graphs:
-    try:
-      G,verts = get_source_graph(graph)
-      exp = DirectionalExp(G,verts,directions)
-      exp.planar_exp()
-      #exp.find_num_directions()
-      experiment.append(exp.alpha)
-      pass
-    except Exception as e:
-          print(e.__class__, "occurred.")
-          print(exp.num_directions)
-          #fig, ax = ox.plot_graph(G,node_color='r',show=False, close=False)
-          #plt.show()
-          print("Next entry.")
-
-
-  bins = np.linspace(math.ceil(min(experiment)), 
-                   math.floor(max(experiment)),
-                   20) # fixed number of bins
-
-  plt.xlim([min(experiment), max(experiment)+2])
-
-  plt.hist(experiment, bins=bins, alpha=0.5)
-  plt.title('Alpha Values on 5 Node Subgraphs (10 Directions on Unit Circle)')
-  plt.xlabel('Alpha')
-  plt.ylabel('count')
-
-  plt.show()
-  '''
-
   
-  source_dir = os.path.join("graphs","maps","Bozeman","graphs","5_graphs", "5_nodes.pickle")
-  in_graph = open(source_dir,"rb")
-  graphs = pickle.load(in_graph)
+  d = random.uniform(0, 2*np.pi)
+  print(d)
   
   alphas = []
   experiments = []
   for graph in graphs:
-    i= 1
+    i = 1
     try:
-      directions = circle_disc(i)
+      directions = circle_disc(d,i)
       G,verts = get_source_graph(graph)
       exp = DirectionalExp(G,verts,directions)
       exp.planar_exp()
       exp.find_num_directions()
-      #print(exp.num_directions[-1])
+      
       while exp.num_directions[-1] > 1:
-        #print(i)
         i += 1
-        directions = circle_disc(i)
+        directions = circle_disc(d,i)
         exp = DirectionalExp(G,verts,directions)
         exp.planar_exp()
         exp.find_num_directions()
@@ -197,7 +144,7 @@ if __name__ == "__main__":
       pass
     except Exception as e:
           print(e.__class__, "occurred.")
-          print(exp.num_directions)
+          print(directions)
           #fig, ax = ox.plot_graph(G,node_color='r',show=False, close=False)
           #plt.show()
           print("Next entry.")
@@ -207,63 +154,40 @@ if __name__ == "__main__":
                    math.floor(max(alphas)),
                    20) # fixed number of bins
 
-  plt.xlim([min(alphas), max(alphas)+2])
+  plt.xlim([min(alphas)-2, max(alphas)+2])
 
   plt.hist(alphas, bins=bins, alpha=0.5)
-  plt.title('Alpha Values on 5 Node Subgraphs (Equally Spaced Directions on Unit Circle)')
+  plt.title('Alpha Values on 4 Node Subgraphs (Random Equally Spaced Directions on Unit Circle)')
   plt.xlabel('Alpha (Number of Directions)')
   plt.ylabel('count')
 
   plt.show()
 
-  exp_file = os.path.join("graphs","maps","Bozeman","graphs","5_graphs", "5_nodes_exp.pickle")
+  exp_file = os.path.join("graphs","maps","Bozeman","graphs","4_graphs", "4_nodes_exp_rand_4.pickle")
   with open(exp_file, "wb") as f:
       pickle.dump(experiments, f)
-  
+
   
   '''
+  r = random.uniform(0, 2*np.pi)
+  print(r)
+
+  direc = disc(r, 1)
+  print(direc)
+
   source_dir = os.path.join("graphs","maps","Bozeman","graphs","4_graphs", "4_nodes.pickle")
   in_graph = open(source_dir,"rb")
   graphs = pickle.load(in_graph)
-  planar_dgms = []
-  directions = circle_disc(4)
-  #print(directions)
-  
-  
-  directions = [(1.0,0.0),(np.sqrt(3)/2, 1/2), (np.sqrt(2)/2,np.sqrt(2)/2), 
-                (1/2,np.sqrt(3)/2),(0,1),(-1/2,np.sqrt(3)/2),(-np.sqrt(2)/2,np.sqrt(2)/2),
-                (-np.sqrt(3)/2, 1/2), (-1,0),(-np.sqrt(3)/2, -1/2),(-np.sqrt(2)/2,-np.sqrt(2)/2),
-                (-1/2,-np.sqrt(3)/2), (0,-1),(1/2,-np.sqrt(3)/2),(-np.sqrt(2)/2,-np.sqrt(2)/2),
-                (np.sqrt(3)/2, -1/2)]
-  
-  direction = [(1.0,0.0)]
-  print(direction)
-  
+
+
   G,verts = get_source_graph(graphs[3])
-  exp = DirectionalExp(G,verts,directions)
+  exp = DirectionalExp(G,verts,[(-0.6141074713467367, -0.7892224107538488)])
   exp.planar_exp()
   exp.find_num_directions()
-  #exp.plot_equal_graphs()
-  #exp.plot_num_directions()
-  print(exp.alpha)
-  print(exp.num_directions)
-
-  
-  for pg in exp.planar_graphs:
-    p = PlanarGraph(pg,directions)
-    p.build_diagrams()
-    planar_dgms.append(p)
-
-  print(len(planar_dgms))
-  for p_d in planar_dgms:
-    if functools.reduce(lambda i, j : i and j, map(lambda m, k: m == k, p_d.diagrams, exp.directional_diagrams), True):  
-      print ("The lists are identical") 
-    else: 
-      print ("The lists are not identical") 
   '''
-
-
   
+  
+
 
 
 
