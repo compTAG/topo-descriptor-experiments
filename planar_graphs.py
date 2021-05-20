9343cbb7386e1d30c3622a6eafa89cfffc3c199f
James-B-Wilson <jbwilson1982@gmail.com>
2021-05-20 12:08:02 -0600

    Update experiment files

--- planar_graphs.py
+++ planar_graphs.py
@@ -5,7 +5,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import math
 import os
-import planarity as p
 import itertools
 from shapely.geometry import LineString, Point
 import pickle
@@ -24,23 +23,30 @@ def create_graph(verts, edges):
     g.add_edges_from(edges)
 
     return g
+    
+def save_city_map(city, state, country): 
+  G = ox.graph_from_place(','.join([city, state, country]), simplify=False, network_type='drive')
+  fig = plt.figure()    
+  fig, ax = ox.plot_graph(G,node_color='r',show=False, close=False, bgcolor="white")
+  fig.savefig(os.path.join("graphs","maps",city, city + "_map.pdf"), bbox_inches='tight')
 
 def get_city_map(city,state, country):
-    try:
-    # Create target Directory
-        dirname = os.path.join("graphs","maps",city)
-        os.mkdir(dirname)
-        print("Directory" , dirname ,  "Created ")
-    except FileExistsError:
-        print("Directory" , dirname ,  "already exists")
 
+    dirname = os.path.join("graphs","maps",city,"experiments")
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+        print("Directory %s created" % dirname)
+    else:    
+        print("Directory %s already exists" % dirname)
 
     G = ox.graph_from_place(','.join([city, state, country]), simplify=False, network_type='drive')
     #G = G.to_undirected()
     G_proj = ox.project_graph(G)
     nodes_proj, gdf_edges = ox.graph_to_gdfs(G_proj, edges=True, nodes = True)
 
-    nx.write_gpickle(G, os.path.join("graphs","maps",city,city)+".pkl")
+    nx.write_gpickle(G, os.path.join("graphs","maps",city,city + "_" + state+".pkl"))
+
+    return G, nodes_proj
     
 def request_graph(location_point, dist):
   G = ox.graph_from_point(location_point, dist=dist,simplify=False)
@@ -118,6 +124,89 @@ def circle_disc(d,n):
         directions.append((x, y))
     return directions
 
+def collinear(vertices):
+    d = np.array([[1,1,1], 
+        [vertices[0][0], vertices[1][0], vertices[2][0]], 
+        [vertices[0][1], vertices[1][1], vertices[2][1]]])
+    return 0.5*abs(np.linalg.det(d))
+
+def make_bounding_box(vertex, meters):
+    lat = vertex[0]
+    lon = vertex[1]
+
+    #Radius of the earth
+    r = 6371
+    #meters in kilometers
+    m = meters/1000
+
+    north = lat + (m/r)*(180/np.pi)
+    south = lat - (m/r)*(180/np.pi)
+    east = lon + (m/r)*(180/np.pi)/np.cos(lat*np.pi/180)
+    west = lon - (m/r)*(180/np.pi)/np.cos(lat*np.pi/180)
+
+    return [north,south, east, west]
+
+
+def duplicate_graphs(g, graphs, duplicate = False):
+    for graph in graphs:
+        if set(g.nodes()) == set(graph.nodes()) and set(g.edges()) == set(graph.edges()):
+            print("Duplicate Found")
+            duplicate = True
+            break
+    return duplicate
+
+
+
+def find_subgraphs(G, nodes_proj, source_dir, bbox):
+    graphs4 = []
+    graphs5 = []
+    graphs6 = []
+   #Get node positions
+    x = nodes_proj['lat'].tolist()
+    y = nodes_proj['lon'].tolist()
+    vertices = list(zip(x, y))
+    k = 0
+    for vertex in vertices:
+        try:
+            bb = make_bounding_box(vertex, bbox)
+            g = ox.truncate.truncate_graph_bbox(G, bb[0], bb[1], bb[2], bb[3])
+            if len(g.nodes()) == 4:
+                if len(graphs4) == 0:
+                    graphs4.append(g)
+                else:
+                 if duplicate_graphs(g,graphs4) == False:
+                    graphs4.append(g)
+                print("4 graphs " + str(len(graphs4)))    
+            elif len(g.nodes()) == 5:
+                if len(graphs5) == 0:
+                    graphs5.append(g)
+                else:
+                 if duplicate_graphs(g,graphs5) == False:
+                    graphs5.append(g)
+                print("5 graphs " + str(len(graphs5)))
+            elif len(g.nodes()) == 6:
+                if len(graphs6) == 0:
+                    graphs6.append(g)
+                else:
+                 if duplicate_graphs(g,graphs6)==False:
+                    graphs6.append(g)
+                print("6 graphs " + str(len(graphs6)))
+            k = k+1
+            print(k)
+            pass      
+        except Exception as e:
+            print(e.__class__, "occurred.")
+            print("Next entry.") 
+    
+    g4 = "Bozeman_4graphs_from_source_"+ str(bbox) +".pickle"
+    g5 = "Bozeman_5graphs_from_source_"+ str(bbox) +".pickle"
+    g6 = "Bozeman_6graphs_from_source_"+ str(bbox) +".pickle"
+    with open(os.path.join(source_dir,g4), "wb") as f:
+        pickle.dump(graphs4, f)
+    with open(os.path.join(source_dir,g5), "wb") as f:
+        pickle.dump(graphs5, f)
+    with open(os.path.join(source_dir,g6), "wb") as f:
+        pickle.dump(graphs6, f)
 
 
 '''
--- too_few.py
+++ too_few.py
@@ -5,15 +5,18 @@ import networkx as nx
 import os
 import topology
 import dionysus as d
-import planarity as p
 import matplotlib.pyplot as plt
-from planar_graphs import get_source_graph, plot_graphs, create_graph, circle_disc, is_intersection, find_planar_graphs
+import matplotlib
+from osmnx_requests import make_bounding_box, find_subgraphs,duplicate_graphs
+from planar_graphs import get_city_map, get_source_graph, plot_graphs, create_graph, circle_disc, is_intersection, find_planar_graphs, collinear
 import itertools
 import pickle
 import glob
 import functools
 import math
 import random
+from matplotlib.font_manager import FontProperties
+from PyPDF2 import PdfFileMerger, PdfFileReader
 
 class DirectionalExp(object):
   def __init__(self, G, verts, directions):
@@ -26,10 +29,16 @@ class DirectionalExp(object):
       self.directions = directions
       self.num_directions = []
       self.alpha = 0
+      self.test_collinearity = {}
+
+    
+  def collinear_points(self):
+    vertcombinations = set(itertools.combinations(self.verts, 3))
+    for vertices in vertcombinations:
+      self.test_collinearity[collinear(vertices)] = vertices
 
   def plot_planar(self):
     plot_graphs(self.graphs[1:-1])
-
 
   def clean(self):
     self.graphs.pop(0)
@@ -99,6 +108,7 @@ class DirectionalExp(object):
     self.clean()
     self.create_diagrams()
     self.fill()
+    
 
   def bottlenecks(self):
     dgm_1 = self.directional_diagrams[0]
@@ -110,88 +120,118 @@ class ExpGraph(object):
       self.graph = graph
       self.count = 0
 
+def bar_charts(graphs_file, alphas, num_verts):
+  #Set bar chart properties
+  font = FontProperties()
+  font.set_family('serif')
+  font.set_name('Times New Roman')
+  #font.set_weight('bold')
 
-if __name__ == "__main__":
-  
-  source_dir = os.path.join("graphs","maps","Bozeman","graphs","Source", "Bozeman_graphs_from_source_6.pickle")
-  in_graph = open(source_dir,"rb")
-  graphs = pickle.load(in_graph)
+  f = plt.figure()
+  labels, counts = np.unique(alphas, return_counts=True)
+  plt.bar(labels, counts, align='center', color = "black")
+  plt.gca().set_xticks(range(max(alphas)+1))
+  plt.title('Directions on ' + str(num_verts) + ' Vertex Subgraphs',fontsize=26, fontproperties=font)
+  plt.xlabel('Number of Directions',fontsize=24, fontproperties=font)
+  plt.ylabel('Number of Subgraphs',fontsize=24, fontproperties=font)
+
+  f.savefig(os.path.join("graphs","maps","Bozeman","experiments", str(num_verts)+"_graphs", str(num_verts)+"_vert_exp","exp-small-graph-"+ str(num_verts) +"-vertex-directions_" + str(bbox)+ ".pdf"), bbox_inches='tight')
+
+
+def run_experiment(graphs_file, num_verts, bbox):
+  random.seed(41821)
+  #in_graphs = glob.glob(graph_files + "/*.pickle")
+  in_file = graphs_file
+  in_graphs = open(in_file,"rb")
+  graphs = pickle.load(in_graphs)
   print(len(graphs))
-  
-  
   d = random.uniform(0, 2*np.pi)
   print(d)
-  
+  k = 0
   alphas = []
   experiments = []
   for graph in graphs:
     i = 1
+    print(k)
     try:
+      #in_graph = open(file,"rb")
+      #graph = pickle.load(in_graph)
       directions = circle_disc(d,i)
       G,verts = get_source_graph(graph)
+      print(G.nodes())
+      #fig, ax = ox.plot_graph(G,node_color='r',show=False, close=False)
+      #plt.show()
       exp = DirectionalExp(G,verts,directions)
       exp.planar_exp()
       exp.find_num_directions()
-      
       while exp.num_directions[-1] > 1:
         i += 1
         directions = circle_disc(d,i)
         exp = DirectionalExp(G,verts,directions)
         exp.planar_exp()
         exp.find_num_directions()
+      exp.collinear_points()
       experiments.append(exp)
       alphas.append(exp.alpha)
+      k += 1
       pass
     except Exception as e:
           print(e.__class__, "occurred.")
-          print(directions)
-          #fig, ax = ox.plot_graph(G,node_color='r',show=False, close=False)
-          #plt.show()
           print("Next entry.")
 
+  #Make Bar Chart
+  bar_charts(graphs_file,alphas,num_verts,bbox)
 
-  bins = np.linspace(math.ceil(min(alphas)), 
-                   math.floor(max(alphas)),
-                   20) # fixed number of bins
-
-  plt.xlim([min(alphas)-2, max(alphas)+2])
-
-  plt.hist(alphas, bins=bins, alpha=0.5)
-  plt.title('Alpha Values on 6 Node Subgraphs (Random Equally Spaced Directions on Unit Circle)')
-  plt.xlabel('Alpha (Number of Directions)')
-  plt.ylabel('count')
-
-  plt.show()
-
-  exp_file = os.path.join("graphs","maps","Bozeman","graphs","6_graphs", "6_nodes_exp_source.pickle")
+  #Save Experiment       
+  exp_file = os.path.join("graphs","maps","Bozeman","experiments", str(num_verts)+"_graphs", str(num_verts)+"_vert_exp", str(num_verts) +'_nodes_experiment_' + str(bbox) +'.pickle')
   with open(exp_file, "wb") as f:
       pickle.dump(experiments, f)
 
+
+if __name__ == "__main__":
+
+  city = "Bozeman"
+  state = "MT"
+  country = "USA"
+  vertices = 6
+  bbox = 30
+  source_dir = os.path.join("graphs","maps",city,"experiments")
   
-  '''
-  r = random.uniform(0, 2*np.pi)
-  print(r)
-
-  direc = disc(r, 1)
-  print(direc)
-
-  source_dir = os.path.join("graphs","maps","Bozeman","graphs","4_graphs", "4_nodes.pickle")
-  in_graph = open(source_dir,"rb")
-  graphs = pickle.load(in_graph)
+  #Get subgraphs
+  #G, project_nodes = get_city_map(city,state, country)
+  #find_subgraphs(G, project_nodes, source_dir, bbox)
 
 
-  G,verts = get_source_graph(graphs[3])
-  exp = DirectionalExp(G,verts,[(-0.6141074713467367, -0.7892224107538488)])
-  exp.planar_exp()
-  exp.find_num_directions()
-  '''
-  
-  
-
-
-
-
+  #In graphs location
+  #graphs_file = "Bozeman_" + str(vertices) + "graphs_from_source.pickle"
+  #run_experiment(os.path.join(source_dir,graphs_file),vertices,bbox)
   
 
+  
+  city = "Bozeman"
+  in_file = os.path.join("graphs","maps","Bozeman","experiments", str(vertices)+"_graphs", str(vertices)+"_vert_exp", str(vertices) +'_nodes_experiment_' + str(bbox) + '.pickle')
+  in_exps = open(in_file,"rb")
+  exps = pickle.load(in_exps)
 
   
+  for e in exps:
+    if e.alpha >= 5:
+      val_map = {}
+      collinear_val = min(e.test_collinearity.keys())
+      for node in e.test_collinearity.get(collinear_val):
+        val_map[list(e.pos.keys())[list(e.pos.values()).index(node)]] = 'white'
+    
+      
+      values = [val_map.get(node, 'red') for node in e.graph.nodes()]
+      font = FontProperties()
+      font.set_family('serif')
+      font.set_name('Times New Roman')
+      f = plt.figure()
+      plt.title(str(e.alpha) + " Directions ")
+      nx.draw(e.graph, e.pos, edgecolors='black', node_size = 50, cmap=plt.get_cmap('hot'), node_color=values)
+      #plt.show()
+      f.savefig(os.path.join("graphs","maps","Bozeman","experiments", "collinear_graphs", str(vertices)+"_nodes",str(collinear_val)+".pdf"), bbox_inches='tight')
+  
+
+
+
